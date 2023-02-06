6d189d058b643671a3389e84d8a9346db861ac18
mrv971 <eamilemarvrick13@gmail.com>
2023-02-06 09:19:59 -0400

    selected heroes (wip)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule,
